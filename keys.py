589466db8f41120ef94acfb20c7954d1c60a2d8b
Thony-gans <75247911+thony-gans@users.noreply.github.com>
2022-02-22 11:00:17 +0700

    Add files via upload

---
+++ keys.py
@@ -0,0 +1,216 @@
+#python_installer:
+#install_kali_linux_on_android:
+#Creator : S1k3ys@Xploit
+
+#~
+#Tools ini telah diuji coba Dipergunakan Di terminal Android!!
+#Pesan Saya Jangan Di Copas Ya Tinggal Make Aja kok^_^
+#Karena Ini bukan Tools Harian Tapi Tools Penginstallan sekali pakai
+#Jika Ada kesalahan Atau Error Silahkan Tanya Saja Geratis!!!
+#Hubugi Saya ~~> https://tinyurl.com/muusmuv8 (WhattSapp)
+#Terimakasih telah membaca Source Code ini^_^
+#~
+#~
+
+import os
+import sys
+
+print '''\033[31;1m
+
+      ______    _____          ______     _______    ______   ______  _________
+      mmmmmm    mmmmm    ___   mmmmmmm    mmmmmmm    mmmmmm   mmmmmm  mmmmmmmmm
+     mm    mm  mm   mm   mmm   mm    mm   mm    mm  mm    mm    mm    mmmmmmmmm
+    mm    mm  mm        mmm   mm     mm  mm    mm  mm    mm    mm       mmm
+\033[37;1m   mm    mm   mmmmm    ___   mm     mm  mmmmmm    mm    mm    mm       mmm
+  mm    mm       mm   mmm   mm     mm  mm   mm   mm    mm    mm       mmm
+ mm    mm  mm   mm   mmm   mm    mm   mm    mm  mm    mm    mm       mmm
+  mmmmmm    mmmmm          mmmmmmm    mm    mm   mmmmmm   mmmmmm    mmm 
+
+\033[36;1m  (+)\033[33;1m============[\033[32;1mInstaller+Program+CodedByS1k3ysXplot\033[33;1m]==============\033[36;1m(+)
+                                                                                    
+
+\033[34;1m             |1)\033[32;1m Kali Linux\033[34;1m    |2)\033[32;1m Manjaro\033[34;1m    |3)\033[32;1m Debian
+          
+\033[34;1m             |4)\033[32;1m Fedora\033[34;1m        |5)\033[32;1m Alpine\033[34;1m     |6)\033[32;1m Bionic beaver
+          
+\033[34;1m             |7)\033[32;1m Focal Fossa\033[34;1m   |8)\033[32;1m Void\033[34;1m       |9)\033[32;1m Arch Linux
+              
+
+
+'''
+
+keys = raw_input(" OS-Dunia-Internet ~> ")
+
+
+#Easy_Install_Kali-Linux^_^_On_Android
+if keys =="1":
+    print ""
+    print "\033[33;1m [*]\033[37;1m waiting..."
+    os.system("sleep 5")
+    print "\033[33;1m [*]\033[31;1m koneksi internet harus bagus...."
+    os.system("sleep 4")
+    print "\033[33;1m [*]\033[37;1m sedang mengoreksi penginstallan bahan......\033[33;1m"
+    os.system("sleep 3")
+    print ""
+    print "\033[33;1m [*]\033[37;1m sedang menginstall bahan penting.......\033[33;1m"
+    os.system("sleep 3")
+    os.system("wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Kali/kali.sh -O kali.sh")
+    print ""
+    print ""
+    print "\033[33;1m [*]\033[37;1m sedang menjalankan support......\033[31;1m"
+    os.system("sleep 4")
+    os.system("chmod +x kali.sh && bash kali.sh")
+
+#Easy_Install_Manjaro^_^_On_Android
+if keys =="2":
+    print ""
+    print "\033[33;1m [*]\033[37;1m waiting..."
+    os.system("sleep 5")
+    print "\033[33;1m [*]\033[31;1m koneksi internet harus bagus...."
+    os.system("sleep 4")
+    print "\033[33;1m [*]\033[37;1m sedang mengoreksi penginstallan bahan......\033[33;1m"
+    os.system("sleep 3")
+    print ""
+    print "\033[33;1m [*]\033[37;1m sedang menginstall bahan penting.......\033[33;1m"
+    os.system("sleep 3")
+    os.system("wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Manjaro/manjaro.sh -O manjaro.sh")
+    print ""
+    print ""
+    print "\033[33;1m [*]\033[37;1m sedang menjalankan support......\033[31;1m"
+    os.system("sleep 4")
+    os.system("chmod +x manjaro.sh && bash manjaro.sh")
+
+#Easy_Install_Debian^_^_On_Android
+if keys =="3":
+    print ""
+    print "\033[33;1m [*]\033[37;1m waiting..."
+    os.system("sleep 5")
+    print "\033[33;1m [*]\033[31;1m koneksi internet harus bagus...."
+    os.system("sleep 4")
+    print "\033[33;1m [*]\033[37;1m sedang mengoreksi penginstallan bahan......\033[33;1m"
+    os.system("sleep 3")
+    print ""
+    print "\033[33;1m [*]\033[37;1m sedang menginstall bahan penting.......\033[33;1m"
+    os.system("sleep 3")
+    os.system("wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Debian/debian.sh -O debian.sh")
+    print ""
+    print ""
+    print "\033[33;1m [*]\033[37;1m sedang menjalankan support......\033[31;1m"
+    os.system("sleep 4")
+    os.system("chmod +x debian.sh && bash debian.sh")
+
+#Easy_Install_Fedora^_^_On_Android
+if keys =="4":
+    print ""
+    print "\033[33;1m [*]\033[37;1m waiting..."
+    os.system("sleep 5")
+    print "\033[33;1m [*]\033[31;1m koneksi internet harus bagus...."
+    os.system("sleep 4")
+    print "\033[33;1m [*]\033[37;1m sedang mengoreksi penginstallan bahan......\033[33;1m"
+    os.system("sleep 3")
+    print ""
+    print "\033[33;1m [*]\033[37;1m sedang menginstall bahan penting.......\033[33;1m"
+    os.system("sleep 3")
+    os.system("wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Fedora/fedora.sh -O fedora.sh")
+    print ""
+    print ""
+    print "\033[33;1m [*]\033[37;1m sedang menjalankan support......\033[31;1m"
+    os.system("sleep 4")
+    os.system("chmod +x fedora.sh && bash fedora.sh")
+
+#Easy_Install_Alpine^_^_On_Android
+if keys =="5":
+    print ""
+    print "\033[33;1m [*]\033[37;1m waiting..."
+    os.system("sleep 5")
+    print "\033[33;1m [*]\033[31;1m koneksi internet harus bagus...."
+    os.system("sleep 4")
+    print "\033[33;1m [*]\033[37;1m sedang mengoreksi penginstallan bahan......\033[33;1m"
+    os.system("sleep 3")
+    print ""
+    print "\033[33;1m [*]\033[37;1m sedang menginstall bahan penting.......\033[33;1m"
+    os.system("sleep 3")
+    os.system("wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Alpine/alpine.sh -O alpine.sh")
+    print ""
+    print ""
+    print "\033[33;1m [*]\033[37;1m sedang menjalankan support......\033[31;1m"
+    os.system("sleep 4")
+    os.system("chmod +x alpine.sh && bash alpine.sh://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Alpine/alpine.sh | bash")
+
+#Easy_Install_Ubuntu^_^_On_Android
+if keys =="6":
+    print ""
+    print "\033[33;1m [*]\033[37;1m waiting..."
+    os.system("sleep 5")
+    print "\033[33;1m [*]\033[31;1m koneksi internet harus bagus...."
+    os.system("sleep 4")
+    print "\033[33;1m [*]\033[37;1m sedang mengoreksi penginstallan bahan......\033[33;1m"
+    os.system("sleep 3")
+    print ""
+    print "\033[33;1m [*]\033[37;1m sedang menginstall bahan penting.......\033[33;1m"
+    os.system("sleep 3")
+    os.system("wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Ubuntu/ubuntu.sh -O ubuntu.sh")
+    print ""
+    print ""
+    print "\033[33;1m [*]\033[37;1m sedang menjalankan support......\033[31;1m"
+    os.system("sleep 4")
+    os.system("chmod +x ubuntu.sh && bash ubuntu.sh")
+
+#Easy_Install_Ubuntu20^_^_On_Android
+if keys =="7":
+    print ""
+    print "\033[33;1m [*]\033[37;1m waiting..."
+    os.system("sleep 5")
+    print "\033[33;1m [*]\033[31;1m koneksi internet harus bagus...."
+    os.system("sleep 4")
+    print "\033[33;1m [*]\033[37;1m sedang mengoreksi penginstallan bahan......\033[33;1m"
+    os.system("sleep 3")
+    print ""
+    print "\033[33;1m [*]\033[37;1m sedang menginstall bahan penting.......\033[33;1m"
+    os.system("sleep 3")
+    os.system("wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Ubuntu20/ubuntu20.sh -O ubuntu20.sh")
+    print ""
+    print ""
+    print "\033[33;1m [*]\033[37;1m sedang menjalankan support......\033[31;1m"
+    os.system("sleep 4")
+    os.system("bash ubuntu20.sh")
+
+#Easy_Install_Void^_^_On_Android
+if keys =="8":
+    print ""
+    print "\033[33;1m [*]\033[37;1m waiting..."
+    os.system("sleep 5")
+    print "\033[33;1m [*]\033[31;1m koneksi internet harus bagus...."
+    os.system("sleep 4")
+    print "\033[33;1m [*]\033[37;1m sedang mengoreksi penginstallan bahan......\033[33;1m"
+    os.system("sleep 3")
+    print ""
+    print "\033[33;1m [*]\033[37;1m sedang menginstall bahan penting.......\033[33;1m"
+    os.system("sleep 3")
+    os.system("wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Void/void.sh")
+    print ""
+    print ""
+    print "\033[33;1m [*]\033[37;1m sedang menjalankan support......\033[31;1m"
+    os.system("sleep 4")
+    os.system("chmod +x void.sh && bash void.sh")
+
+#Easy_Install_Arch-Linux^_^_On_Android
+if keys =="9":
+    print ""
+    print "\033[33;1m [*]\033[37;1m waiting..."
+    os.system("sleep 5")
+    print "\033[33;1m [*]\033[31;1m koneksi internet harus bagus...."
+    os.system("sleep 4")
+    print "\033[33;1m [*]\033[37;1m sedang mengoreksi penginstallan bahan......\033[33;1m"
+    os.system("sleep 3")
+    print ""
+    print "\033[33;1m [*]\033[37;1m sedang menginstall bahan penting.......\033[33;1m"
+    os.system("sleep 3")
+    os.system("wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Arch/armhf/arch.sh -O arch.sh")
+    print ""
+    print ""
+    print "\033[33;1m [*]\033[37;1m sedang menjalankan support......\033[31;1m"
+    os.system("sleep 4")
+    os.system("chmod +x arch.sh && bash arch.sh")
+
+#End
